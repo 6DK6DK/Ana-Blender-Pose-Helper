c455f274cbbe34b5c87e1dc10f68669447e813da
60000 <6dee000@gmail.com>
2021-12-11 18:26:02 -0600

    added name map bypass for post 2021-11-13 Ana files

--- operators.py
+++ operators.py
@@ -45,23 +45,33 @@ class LoadAnaBone(bpy.types.Operator):
     def execute(self, context):
         arm = context.scene.anamnesis_armature.pose
 
-        # v1.0.1: old code for opening library file left in comment. I think the below will be more reliable, but I've used it less myself.
-        # with open(bpy.utils.script_path_user() + '\\addons\\Pose_Helper\\map.json', 'r') as f:
         with open(path.join(path.dirname(__file__), 'map.json'), 'r') as f:
             name_map = json.load(f)
 
         with open(self.path, 'r') as f:
             pose = json.load(f)['Bones']
-        
+
+        # check if the first bone in the pose file is in the map to determine legacy
+        legacy = False
+        if list(pose.keys())[0] in name_map.values():
+            legacy = True
+
         # !!! The meat.
         bone = arm.bones[self.bone]
-        if bone.name in name_map and name_map[bone.name] in pose:
-            rot = pose[name_map[bone.name]]["Rotation"].split(", ")
+        # fucky legacy check switch thing to avoid redundant code
+        rot = False
+        if legacy and bone.name in name_map and name_map[bone.name] in pose:
+            rot = pose[name_map[bone.name]]
+        elif bone.name in pose:
+            rot = pose[bone.name]
+
+        if rot:
+            rot = rot["Rotation"].split(", ")
             rot = [float(x) for x in rot]
             # .pose is XYZW, we need to switch to WXYZ
             rot.insert(0, rot.pop())
             rot = Quaternion(rot)
-         
+
             # key blender function for transforming from the character's space to the bone's space relative to rest.
             bone.rotation_quaternion = context.scene.anamnesis_armature.convert_space(pose_bone = bone, matrix = rot.to_matrix().to_4x4(), from_space = 'POSE', to_space = 'LOCAL').to_quaternion()
         
